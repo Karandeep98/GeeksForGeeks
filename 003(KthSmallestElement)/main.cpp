aded673e5ebcded4ccd8147791b48e077e671e16
Karandeep Singh <karandeep041998@gmail.com>
2020-05-08 20:05:05 +0530

    minor fixes

--- 003(KthSmallestElement)/main.cpp
+++ 003(KthSmallestElement)/main.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -20,7 +19,5 @@ int main()
         cout<<A[k-1]<<endl;
          t--;
     }
-
-
     return 0;
 }
